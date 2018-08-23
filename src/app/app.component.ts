6a284a7543113445fc444da835d7f73cb2226bf8
ledzappa <juntti.mikael@gmail.com>
2018-08-23 19:34:31 +0200

    Keeping track of failed generate-attempts in order to avoid an infinite loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,12 @@ export class AppComponent implements OnInit {
   		'001001' // klarna
   	];
 
+  	error: string = '';
   	pIndex = 0;
   	noRepeat = 'false';
   	numOfWords = 100;
+  	startingPoint = '';
+  	wordsGenerated = 0;
   	
   	consonants: string[] = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
     vowels: string[] = ['a', 'e', 'i', 'o', 'u', 'y'];
@@ -34,16 +37,26 @@ export class AppComponent implements OnInit {
   		// console.log(this.noRepeat);
   		// console.log('pIndex: ' + pIndex);
   		// console.log('numOfWords: ' + numOfWords);
+  		this.error = '';
   		this.words = [];
+  		this.wordsGenerated = 0;
+  		let failedAttempts = 0;
         let word = '';
 
         let i = 0;
-        while (i < this.numOfWords) {
-        	word = '';
+        while (i < this.numOfWords && failedAttempts < 1000) {
+        	let timeoutHandle = setTimeout(() => {return false;}, 5000);
+
+        	if (this.startingPoint.length > 0) {
+        		word = this.startingPoint;
+        	} else {
+        		word = '';
+        	}
+
         	let j = 0;
-	        while (j < this.patterns[this.pIndex].length) {
+	        while (j < (this.patterns[this.pIndex].length - this.startingPoint.length)) {
 	        	let letter = '';
-	        	if (this.patterns[this.pIndex].charAt(j) == '0') {
+	        	if (this.patterns[this.pIndex].charAt(j + this.startingPoint.length) == '0') {
 	        		letter = this.consonants[Math.floor(Math.random() * this.consonants.length)];
 	        	} else {
 	        		letter = this.vowels[Math.floor(Math.random() * this.vowels.length)];
@@ -64,10 +77,17 @@ export class AppComponent implements OnInit {
             // push if word hasn't already been generated
             if (this.words.indexOf(word) === -1) {
             	this.words.push(word);
+            	failedAttempts = 0;
             	i++;
+            } else {
+            	failedAttempts++;
             }
         }
 
-        console.log(this.words);
+        if (failedAttempts == 1000) {
+            this.error = 'Whops! Ran out of words :( (' + this.words.length + ' generated)';
+        }
+      
   	}
+
 }
